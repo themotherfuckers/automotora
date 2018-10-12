3cf8f454e7ccc5ca02b3c5d6af7b958ffe69c423
Your name <br.andrades@alumnoduoc.cl>
2018-10-12 19:06:40 -0400

    funcionalidad login lista

--- automotora/urls.py
+++ automotora/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    path('account/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
